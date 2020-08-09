1c95d39fb856c3184c36c073ecbabec5276ce2d8
farzad <66000207+farzad-or@users.noreply.github.com>
2020-08-09 18:58:50 +0430

    session

--- app.js
+++ app.js
@@ -9,18 +9,56 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////setting up session////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+app.use(session({
+	key:"user_sid",
+	secret:"somerandomstuff",
+	resave: false,
+	saveUninitialized: false,
+    cookie: {
+		expires: 600000,
+    }
+}));
+
+
+
+app.use(cookieParser());
+
+app.use(function(req, res, next) {
+	if (req.cookies.user_sid && !req.session.user) {
+		res.clearCookie("user_sid");
+	};
+
+	next();
+});
+
+
+app.use(function(req, res, next){
+	if(!req.cookies.lang){
+		req.cookies.lang="FA";
+		req.cookies.theme="light"
+		res.cookie("lang","FA")
+		res.cookie('theme',"light" );
+	}
+	
+	next();
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////view engine setup/////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', authentication);
-app.use('/users', usersRouter);
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////data base setup///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 // handle mongoose collection.ensureIndex warn
 mongoose.set('useNewUrlParser', true);
@@ -32,7 +70,25 @@ useUnifiedTopology: true
 });
 
 
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////end points and midlles wares/////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', authentication);
+app.use('/users', usersRouter);
+
+
+
 // module.exports = app;
 app.listen(3000,function(){
-console.log('app run 3000');
+console.log('up and running on 3000');
 })
